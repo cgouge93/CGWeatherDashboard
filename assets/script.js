08734c40df790ce6eae3377bb4909f1cc7d34260
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-01-18 11:56:51 -0600

    added UV Index APi call & formatting

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,58 @@
+// Display current weather
 function searchCurrentWeather(city) {
     var city = $("#city").val().trim()
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6c07134d45d67e2aa2498bb5c00f8693";
-    console.log(queryURL)
+    var APIKey = "6c07134d45d67e2aa2498bb5c00f8693"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
  
-   // Logging the URL so we have access to it for troubleshootingdddddddddddddddd
+   // Logging the URL so we have access to it for troubleshooting
   console.log(queryURL)
-  
+ 
+// API call for current city name, icon, temp, humidity, wind speed
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-var currentInfo = JSON.stringify(response);
-
-
 var cityName = response.name
-$("#current-city").text( cityName )
-
 var currentIconCode = response.weather[0].icon
 var iconURL = "http://openweathermap.org/img/w/" + currentIconCode + ".png"
+var temperatureF =  (response.main.temp - 273.15) * 1.80 + 32;
+var humidity = response.main.humidity
+var windSpeed = response.wind.speed
+var lat = response.coord.lat;
+var long = response.coord.lon;
+var indexQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIKey
+
+$("#current-city-wind").html("<p>Wind Speed: " + windSpeed + " MPH")
+$("#current-city-humidity").html("<p>Humidity: " + humidity + "%</p>")
+$("#current-city-temp").html("<p>Temperature: " + temperatureF.toFixed(1) + "&deg; F</p>")
+$("#current-city").text( cityName )
 $("#wicon").attr("src", iconURL)
 
-var temperatureF =  (response.main.temp - 273.15) * 1.80 + 32;
-$("#current-city-temp").html("<p>Temperature: " + temperatureF.toFixed(1) + "&deg; F</p>")
-
-var humidity = response.main.humidity
-$("#current-city-humidity").html("<p>Humidity: " + humidity + "%</p>")
-
-var windSpeed = response.wind.speed
-$("#current-city-wind").html("<p>Wind Speed: " + windSpeed + " MPH")
-
-
+ console.log(indexQueryURL)
+// API call for current UV index
+$.ajax({
+    url: indexQueryURL,
+    method: "GET",
+  }).then(function(response) {
+    var UVIndex = response.value
+    
+    $("#current-city-UV").text("UV Index: ");
+    UVIndexValue = document.createElement("span")
+    UVIndexValue.textContent = UVIndex
+    if (UVIndex <= 2){
+        UVIndexValue.classList = "favorable"
+    } else if (UVIndex > 2 && UVIndex <= 5) {
+        UVIndexValue.classList = "moderate"
+    } else if (UVIndex > 5 && UVIndex <= 7) {
+        UVIndexValue.classList = "high"
+    } else if (UVIndex > 7 && UVIndex <= 10) {
+        UVIndexValue.classList = "severe"
+    } else if (UVIndex >= 11) {
+        UVIndexValue.classList = "extreme"
+    }
+    $("#current-city-UV").append(UVIndexValue)
   });
+});
 }
 
 
@@ -38,6 +60,11 @@ $("#current-city-wind").html("<p>Wind Speed: " + windSpeed + " MPH")
 
 
 
+
+
+
+
+// Click event for search button
 $("#run-search").on("click", function(event) {
     // This line allows us to take advantage of the HTML "submit" property
     // This way we can hit enter on the keyboard and it registers the search
